d96dd0af06f241dae4a8f6658e0c713faf1dbfbb
Opeyemi <thomasopeyemi1@gmail.com>
2025-10-15 10:27:27 +0100

    First Commit

---
+++ get.py
@@ -0,0 +1,24 @@
+# from http.server import BaseHTTPRequestHandler, HTTPServer
+# import json
+
+# data = [
+#     {
+#         "name": "Sam Larry",
+#         "track": "AI Developer"
+#     }
+# ]
+# class BasicAPI(BaseHTTPRequestHandler):
+#     def send_data(self, data, status = 200):
+#         self.send_response(status)
+#         self.send_header("Content-type", "application/json")
+#         self.end_headers()
+#         self.wfile.write(json.dumps(data).encode())
+    
+#     def do_Get(self):
+#         self.send_data(data)
+
+# def run():
+#     HTTPServer(('localhost', 8000), BasicAPI).serve_forever()
+
+# print("Application is running ")
+# run()
---
+++ main.py
@@ -0,0 +1,28 @@
+# # from fastapi import FastAPI
+# # from pydantic import BaseModel
+
+# # app = FastAPI()
+
+# # # Data model
+# # class User(BaseModel):
+# #     name: str
+# #     age: int
+
+# # # Routes
+# # @app.get("/")
+# # def home():
+# #     return {"message": "Welcome to FastAPI"}
+
+# # @app.post("/users")
+# # def create_user(user: User):
+# #     return {"message": f"User {user.name} (age {user.age}) created successfully!"}
+
+
+
+
+# from fastapi import FastAPI
+# app = FastAPI()
+
+# @app.get("/")
+# def read_root():
+#     return {"Message": f"Hello, FastAPI"}
